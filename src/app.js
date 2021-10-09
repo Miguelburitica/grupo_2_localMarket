196f79414a3f26d7f5a75acefbe0bb9019af0506
dmelisadg <dmdoming@gmail.com>
2021-10-09 16:17:11 -0500

    Modified header: html and css. Add: Normalized.css

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname__, "./public")))
+app.use(express.static(path.resolve(__dirname, "./public")))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/header_noLogueado.html'))});
 
 app.listen(port, () => console.log("Servidor corriendo"))
