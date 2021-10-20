f8eeaeaf1c9b6b0968319ae161492f248aabf3c4
Miguelburitica <79605328+Miguelburitica@users.noreply.github.com>
2021-10-19 23:05:08 -0500

    add: product-detail.html

--- src/app.js
+++ src/app.js
@@ -9,4 +9,8 @@ app.use(express.static(path.resolve(__dirname, "./public")))
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/index.html'))});
 
+app.get('/product-detail', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/product-detail.html'))
+});
+
 app.listen(port, () => console.log("Servidor corriendo"))
