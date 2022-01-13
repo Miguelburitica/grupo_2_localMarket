e65dd372dbace993fb3716e1484da67f4530b404
dmzapatas1 <dmzapatas10@gmail.com>
2022-01-12 21:02:33 -0500

    Add: user model

---
+++ src/database/models/User.js
@@ -0,0 +1,69 @@
+module.exports = (sequelize, dataType) => {
+	let cols = {
+		id: {
+			type: dataType.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		user_name: {
+			type: dataType.STRING,
+			allowNull: false,
+		},
+		names: {
+			type: dataType.STRING,
+			allowNull: false,
+		},
+		surname: {
+			type: dataType.STRING,
+			allowNull: false,
+		},
+		email: {
+			type: dataType.STRING,
+			allowNull: false,
+            unique: true,
+		},
+		phone: {
+			type: dataType.INTEGER,
+			allowNull: false,
+		},
+		password: {
+			type: dataType.STRING,
+			allowNull: false,
+		},
+		photo: {
+			type: dataType.STRING,
+			allowNull: false,
+		},
+		agree_terms: {
+			type: dataType.STRING,
+		},
+		agree_data: {
+			type: dataType.STRING,
+		},
+        rols_id: {
+			type: dataType.BOOLEAN,
+            allowNull: false,
+		},
+	};
+
+	let config = {
+		tableName: 'users',
+		timestamps: false,
+	};
+
+    const User = sequelize.define('User', cols, config)
+
+    User.associate = function (modelos) {
+		User.hasMany(modelos.Market, {
+			as: 'market',
+			foreignKey: 'users_id',
+		})
+		
+		// User.hasOne(modelos.Rol, {
+		// 		as: 'rol',
+		// 		foreignKey: 'users_id',
+		// 	})
+			
+		}
+    return User;
+};
