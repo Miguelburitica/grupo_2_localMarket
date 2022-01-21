3a8a294269468e27911b0e8b3080c595bbabc5e3
dmelisadg <dmdoming@gmail.com>
2022-01-20 22:37:20 -0500

    Add: CRUD Sellers

--- src/model/seller.model.js
+++ src/model/seller.model.js
@@ -1,34 +1,96 @@
+// let env = process.env
+
+// env.USER = 'root'
+// env.DATABASE_PASSWORD = '1234'
+// env.DATABASE_NAME = 'LocalMarket'
+// env.DATABASE_HOST = '127.0.0.1'
+// env.DATABASE_PORT = '3307'
+
 const path = require('path');
-const fs = require('fs');
-const dataSellers = require('../data/sellers.json');
-// path Seller JSON
-const sellerFilePath = path.resolve(__dirname, '../data/sellers.json');
+
+// const fs = require('fs');
+// const dataSellers = require('../data/sellers.json');
+
+const db = require( path.resolve(__dirname, '../database/models'));
 
 const model = {
 	// Obtener todos los sellers
-	getSellers: function () {
-		return JSON.parse(fs.readFileSync(sellerFilePath), 'utf-8');
+	getSellers: async function () {
+		// return JSON.parse(fs.readFileSync(sellerFilePath), 'utf-8');
+		try{
+			let seller = await db.User.findAll({
+				include:['rol'],
+				where:{
+					rols_id:1,
+				}
+			});
+			return seller;
+		}catch(err){
+			console.log(err)
+			return err
+		}
 	},
 	// Encontrar un seller por email
-	findByEmailSeller: function (email) {
-		const allSellers = this.getSellers();
-		const foundSeller = allSellers.find((item) => item.email === email);
+	findByEmailSeller: async function (email) {
+		try {
+		const allSellers = await this.getSellers();
+		const foundSeller = await allSellers.find((item) => item.email === email);
 		return foundSeller;
-	},
+	}catch (err){
+        console.log(err)
+        return err;
+	}
+},
 	// Actualizar el JSON de sellers
 	updateSellers: function (sellers) {
 		fs.writeFileSync(sellerFilePath, JSON.stringify(sellers, null, 4));
 	},
-	//Obtener el nuevo ID seller
-	newSellerId: function () {
-		let ultimo = 0;
-		this.getSellers().forEach((seller) => {
-			if (seller.id > ultimo) {
-				ultimo = seller.id;
-			}
-		});
-		return ultimo + 1;
+	createSeller: async function(user){
+	try {
+	let newUser =
+		{
+        user_name:user.user_name,
+        names:user.names,
+        surname:user.surname,
+        email:user.email,
+        phone:user.phone,
+        password:user.password,
+        photo:user.photo,
+        rols_id:1
+		}
+	await db.User.create(newUser)
+	} catch (err){
+		console.log(err)
+        return err;
+	}
 	},
+
+	//Obtener el nuevo ID seller
+	// newSellerId: function () {
+	// 	let ultimo = 0;
+	// 	this.getSellers().forEach((seller) => {
+	// 		if (seller.id > ultimo) {
+	// 			ultimo = seller.id;
+	// 		}
+	// 	});
+	// 	return ultimo + 1;
+	// },
 };
 
-module.exports = model;
+// module.exports = model;
+let newuser =
+{
+user_name:'dianita',
+names:'Diana',
+surname:'Dominguez',
+email:'dianita@gmail.com',
+phone:'125643987',
+password:'D1234567890z',
+photo:'user.photo',
+}
+async function Mostrar(){
+	let x= await model.createSeller(newuser)
+	console.log(x)
+  }
+  
+  Mostrar()
